f9f889d2db7ccd9606263cda7426ea0e8835ffde
JuLioVera <92015093+JuLiio77@users.noreply.github.com>
2024-02-22 16:44:49 +0100

    subir back

---
+++ facuras/src/main/java/com/grupo/facuras/FacurasApplication.java
@@ -0,0 +1,13 @@
+package com.grupo.facuras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacurasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacurasApplication.class, args);
+	}
+
+}
---
+++ facuras/src/test/java/com/grupo/facuras/FacurasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo.facuras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacurasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
