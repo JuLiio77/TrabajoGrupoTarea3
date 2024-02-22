a4a28d45b97a4914ac3d128a8cf35698b477ad1a
JoseJavier21 <92015037+JoseJavier21@users.noreply.github.com>
2024-02-22 17:05:40 +0100

    model factura

--- facuras/src/main/java/com/grupo/facuras/model/Facturas.java
+++ facuras/src/main/java/com/grupo/facuras/model/Facturas.java
@@ -1,4 +1,26 @@
 package com.grupo.facuras.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name = "factura")
 public class Facturas {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Double importe_total;
+    private Long identificador;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "clientes_id")
+    private List<Clientes> clientes;
 }
